c13fd3bdc73f488d98af758205039d146aa36cc5
Raisa Anjani <raisa.anjani@gdplabs.id>
2015-03-09 17:10:08 +0700

    Initial Commit

---
+++ podcore/podcore.h
@@ -0,0 +1,19 @@
+//
+//  podcore.h
+//  podcore
+//
+//  Created by Raisa Anjani on 3/9/15.
+//  Copyright (c) 2015 Raisa Anjani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for podcore.
+FOUNDATION_EXPORT double podcoreVersionNumber;
+
+//! Project version string for podcore.
+FOUNDATION_EXPORT const unsigned char podcoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <podcore/PublicHeader.h>
+
+
