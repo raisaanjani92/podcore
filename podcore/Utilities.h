1e1e5b148238e1682fb7bcf9ae2df352cfdd0f0c
Raisa Anjani <raisa.anjani@gdplabs.id>
2015-03-09 17:57:43 +0700

    Add podscpec

---
+++ podcore/Utilities.h
@@ -0,0 +1,15 @@
+//
+//  Utilities.h
+//  podcore
+//
+//  Created by Raisa Anjani on 3/9/15.
+//  Copyright (c) 2015 Raisa Anjani. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Utilities : NSObject
+
++ (void)testPod;
+
+@end
---
+++ podcore/Utilities.m
@@ -0,0 +1,17 @@
+//
+//  Utilities.m
+//  podcore
+//
+//  Created by Raisa Anjani on 3/9/15.
+//  Copyright (c) 2015 Raisa Anjani. All rights reserved.
+//
+
+#import "Utilities.h"
+
+@implementation Utilities
+
++ (void)testPod{
+    NSLog(@"testpod");
+}
+
+@end
